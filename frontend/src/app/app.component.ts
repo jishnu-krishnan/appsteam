9aabf181d0a939222c5fc1b73f975543d951b075
jishnu <jishnukrishnan99@gmail.com>
2021-09-20 17:58:18 +0530

    completed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -57,7 +57,7 @@ export class AppComponent {
     const data={
       name:value
     }
-     console.log('hgj',value);
+    
     this.Service.getMovieDetails(JSON.stringify(data)).subscribe(response => {
       this.details=response
       //console.log(this.details)
